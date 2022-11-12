63aca6128c199b81a227600f982379f74744fae8
NikaProkopeva <78029638+NikaProkopeva@users.noreply.github.com>
2022-11-12 12:46:57 +0000

    Deploying to gh-pages from @ NikaProkopeva/goit-react-hw-05-movies@ee19706b9166cf3bd71719ae44ed99f562dc8132 🚀

---
+++ static/js/NotFoundPage.3521b1be.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[754],{438:function(e,i,r){r.r(i);var s=r(731),t=r(184);i.default=function(){return(0,t.jsxs)("div",{children:[(0,t.jsx)("h2",{children:" Let's try again :) "}),(0,t.jsx)("p",{children:(0,t.jsx)(s.rU,{to:"/",children:"Back"})})]})}}}]);
+//# sourceMappingURL=NotFoundPage.3521b1be.chunk.js.map
